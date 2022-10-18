7e966687fcf39c9d7c6cdb51a20f3ffe71d60a78
Gepardgame <thomas.degg@gmail.com>
2022-10-18 22:42:55 +0200

    Added some scripts

---
+++ shell_scripts/farbcodes.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+schwarz="\e[30m"
+weis="\e[97m"
+
+#dunkele Farben
+d_blau="\e[34m"
+d_gruen="\e[32m"
+d_aqua="\e[36m"
+d_rot="\e[31m"
+d_lila="\e[35m"
+d_gelb="\e[33m"
+d_grau="\e[90m"
+
+#helle Farben
+h_grau="\e[37m"
+h_blau="\e[94 "
+h_gruen="\e[92m"
+h_aqua="\e[96m"
+h_rot="\e[91m"
+h_lila="\e[95m"
+h_gelb="\e[93m"
+
+#dunklere Hintergrundfarbe
+b_schwarz="\e[40m"
+b_rot="\e[41m"
+b_gruen="\e[42m"
+b_orange="\e[43m"
+b_blau="\e[44m"
+b_lila="\e[45m"
+b_aqua="\e[46m"
+b_grau="\e[100m"
+
+#heller Hintergrundfarbe
+b_h_grau="\e[47m"
+b_h_rot="\e[101m"
+b_h_gruen="\e[102m"
+b_gelb="\e[103m"
+b_h_blau="\e[104m"
+b_h_lila="\e[105m"
+b_h_aqua="\e[106m"
+b_weis="\e[107m"
+
+#Besonderheiten
+normal="\e[0m"
+fett="\e[1m"
+kursiv="\e[3m"
+durchgestrichen="\e[9m"
+
+#Linuxbesonderheiten
+reverse="\e[7m"
+blinken="\e[5m"
+versteckt="\e[8m"
